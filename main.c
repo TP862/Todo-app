a1155882986ad3825cd13428924ef8a983cce99d
franc <francoisdestroo@gmail.com>
2025-01-07 20:36:46 +0200

    Finished main menue and menue switching works now

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 int main(void)
 {
-  puts("Hello World");
+  int menue;
+
+  puts("Enter 1 to make a new note or 2 to view existing notes\n");
+
+  scanf("%d",&menue);
+
+  if(menue == 1)
+  {
+    system("clear");
+    puts("Create a new Note!");
+  }
+  else if( menue == 2)
+  {
+    system("clear");
+    puts("A list of your old notes");
+  }
   return 0;
 }
