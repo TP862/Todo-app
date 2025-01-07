001f9cba1355a1106c3685a873b26ae205e05c6e
franc <francoisdestroo@gmail.com>
2025-01-07 21:47:40 +0200

    Segfaults can burn in hell and safe c is hard-a

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX 255
 
 int main(void)
 {
   int menue;
+  char name[MAX];
+  char note[MAX];
+  char file_mode;
+  char file_extension[] = ".txt";
+  FILE* fptr;
 
   puts("Enter 1 to make a new note or 2 to view existing notes\n");
 
@@ -13,6 +21,34 @@ int main(void)
   {
     system("clear");
     puts("Create a new Note!");
+    
+    //Enter The name of the note
+    puts("Name of new note?");
+    scanf("%s",name);
+
+    strcat(name,file_extension);
+    
+    //file handeling bit im cryng
+    file_mode = 'w';
+    
+    FILE* fopen(name,file_mode);
+    
+    if(fptr == NULL)
+    {
+      puts("File Failed to be created! :(");
+      exit(0);
+    }
+    else 
+    {
+      puts("Note Created Succesfully");
+    }
+
+    puts("Make a note!");
+    gets(note);
+    
+    fprintf(fptr,"%s",note);
+
+    fclose(fptr);
   }
   else if( menue == 2)
   {
